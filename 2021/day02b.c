ac1a135455bec276bc333ad8e68c1a29a4c2449d
Kevin Nobel <kevin@2sk.nl>
2021-12-21 22:02:17 +0100

    Implemented solution for Dive! (2021/day2)

---
+++ 2021/day02b.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define MAX_LEN 32
+
+int main() {
+    char buf[MAX_LEN];
+    int32_t horizontal = 0;
+    int32_t depth = 0;
+    int32_t aim = 0;
+
+    /* Read each line from stdin. */
+    while(fgets(buf, MAX_LEN, stdin) != NULL) {
+        /* Check the first character of the line to determine the instruction.
+         * Note that each instruction has a unique firs character! */
+        switch(buf[0]) {
+            case 'f':   int32_t tmp = atoi(&buf[8]);
+                        horizontal += tmp;
+                        depth += aim * tmp;
+                        break;
+            case 'd':   aim += atoi(&buf[5]);
+                        break;
+            case 'u':   aim -= atoi(&buf[3]);
+                        break;
+        }
+    }
+
+    printf("(DAY 02B) Horizontal positon: %d; Depth: %d; Final answer: %d.\n", horizontal, depth, horizontal * depth);
+    return 0;
+}
